17fa7d2e64c72d9dad97ed3100178bfc48c4fb99
praveenalpha <pm495949@gmail.com>
2020-11-05 23:49:37 +0530

    'today'

--- fullStackApp/backend/app.js
+++ fullStackApp/backend/app.js
@@ -1,3 +1,37 @@
-first contribution
-this is another one
-last one
+//npm init -y
+//npm install express
+//npm  install nodemon
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+const { v4: uuidv4 } = require('uuid');
+
+// this is how we get something from UI/postman
+const get = (req,res) => {
+    console.log(req.body);
+    res.send("you are in home");
+};
+app.get("/home", get);
+
+// this is how we post something to UI/postman
+const post =  (req,res) => {
+    let uid = uuidv4();
+    req.body.uid = uid;
+    console.log(uid);
+    res.json({
+        message: "we got post res",
+        body: req.body
+    })
+};
+app.post("/user", post);
+
+// this is how we get something with uid from UI/postman
+app.get("/user", (req,res) => {
+
+})
+
+app.listen(3000, () =>{
+    console.log("started server at port 3000");
+});
