a2af31c66206c4abaacbf5e266bb77ec6d4ca809
praveenalpha <pm495949@gmail.com>
2020-11-13 02:06:05 +0530

    react

---
+++ fullStackApp/frontend/insta-main/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+import './App.css';
+import Header from './components/header';
+function App() {
+  return (<Header/> );
+}
+
+export default App;
---
+++ fullStackApp/frontend/insta-main/src/components/header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './header.css';
+const Header = () => {
+    return ( 
+        <div className="header">
+            <div className="header-image">
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2a/Instagram_logo.svg/1200px-Instagram_logo.svg.png" alt=""/>
+            </div>
+        </div>
+    
+    );
+}
+ 
+export default Header;<h1>HEADER</h1>
