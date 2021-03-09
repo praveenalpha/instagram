b0c603bb9f3721f6e47a24d46b63e7a0a84f24ae
Praveen Mani <48654802+praveenalpha@users.noreply.github.com>
2021-03-09 16:45:35 +0530

    Delete fullStackApp/frontend/insta-main/src directory

--- fullStackApp/frontend/insta-main/src/components/profileList.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-class ProfileList extends Component {
-    state = {  }
-    render() { 
-        return ( <h1>ProfileList</h1> );
-    }
-}
- 
-export default ProfileList;
